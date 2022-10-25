b494608d3539bcd74fc225cf0b23b11928564883
Vince Elizaga <vncelizaga@gmail.com>
2022-10-25 09:20:15 -0400

    add error response

--- src/handlers/main.ts
+++ src/handlers/main.ts
@@ -3,7 +3,7 @@ import { s3 } from '../aws/s3';
 const env:any = process.env;
 
 export const singleFileUpload = async (event) => {
-    let response = {};
+    let response = { statusCode: 0, body: {}, headers: { 'Access-Control-Allow-Origin': '*' } };
 
     const filename = event.headers.filename;
     const binary_file = event.body;
@@ -20,20 +20,23 @@ export const singleFileUpload = async (event) => {
 
         const result = await s3.putObject(params).promise();
 
-        response = {
-            statusCode: 200,
-            headers: { 'Access-Control-Allow-Origin': '*' },
-            body: JSON.stringify({
-                data : {
-                    result, 
-                    status : 'success', 
-                    fileUrl: env.fileRootPath + path
-                }
-            })
-        }
-        
-    } catch (ex) {
-        throw ex;
+        response.statusCode = 200;
+        response.body = JSON.stringify({
+            data : {
+                result, 
+                status : 'success', 
+                fileUrl: env.fileRootPath + path
+            }
+        });
+
+    } catch (err) {
+        response.statusCode = 500;
+        response.body = JSON.stringify({
+            data : {
+                status : 'error', 
+                message : err.message
+            }
+        });
     }
 
     return response;
