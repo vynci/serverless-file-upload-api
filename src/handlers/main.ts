52641f1761336a9f581d63a8252d5c2900ff4f2c
Vince Elizaga <vncelizaga@gmail.com>
2022-10-25 09:14:44 -0400

    update main

--- src/handlers/main.ts
+++ src/handlers/main.ts
@@ -4,28 +4,32 @@ const env:any = process.env;
 
 export const singleFileUpload = async (event) => {
     let response = {};
-    
-    const folderPath = 'test_folder/'
 
     const filename = event.headers.filename;
     const binary_file = event.body;
 
     try {
         const bufferData = new Buffer(binary_file, 'base64');
-        const path = folderPath + `${new Date().getTime()}_${filename}`;
+        const path = env.folderPath + `${new Date().getTime()}_${filename}`;
 
-        let params = {
+        const params = {
             Body: bufferData,
             Bucket: env.fileBucket,
             Key: path
         };
-        
+
         const result = await s3.putObject(params).promise();
 
         response = {
             statusCode: 200,
             headers: { 'Access-Control-Allow-Origin': '*' },
-            body: JSON.stringify({data : { length: event.body.length, result, status : 'success', fileUrl: env.fileRootPath + path}})
+            body: JSON.stringify({
+                data : {
+                    result, 
+                    status : 'success', 
+                    fileUrl: env.fileRootPath + path
+                }
+            })
         }
         
     } catch (ex) {
